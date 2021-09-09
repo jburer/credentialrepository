f506a86db98dd1424eea24349f3d2ee5540fb3b7
jburer <jburer@gmail.com>
2021-09-09 13:49:33 -0400

    .initial

---
+++ AWS/secretsmanager.py
@@ -0,0 +1,92 @@
+# Use this code snippet in your app.
+# If you need more information about configurations or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developers/getting-started/python/
+
+import boto3
+import base64
+from botocore.exceptions import ClientError
+from botocore.exceptions import ProfileNotFound
+
+
+def get_secret():
+
+    account = 'CredentialRepositoryApp'
+    secret_name = "helloworld"
+    region_name = "us-east-1"
+
+    # Establish AWS session
+    try:
+        session = boto3.session.Session(profile_name=account)
+    except ProfileNotFound as err:
+        return 'Failure', err
+
+    # Connect to STS service
+    sts_client = session.client('sts')
+
+    # Call the assume_role method of the STSConnection object and pass the role
+    # ARN and a role session name.
+    assumed_role_object = sts_client.assume_role(
+        RoleArn="arn:aws:iam::562241862267:role/CredentialRepositoryRole",
+        RoleSessionName="AssumeRoleSession1"
+    )
+
+    # From the response that contains the assumed role, get the temporary
+    # credentials that can be used to make subsequent API calls
+    credentials = assumed_role_object['Credentials']
+
+    # Create a Secrets Manager client using assumed role
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name,
+        aws_access_key_id=credentials['AccessKeyId'],
+        aws_secret_access_key=credentials['SecretAccessKey'],
+        aws_session_token=credentials['SessionToken'],
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'AccessDeniedException':
+            # User does not have permissions to get secret value on resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        return e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+        else:
+            secret = base64.b64decode(
+                get_secret_value_response['SecretBinary'])
+        return secret
+
+
+credentials = get_secret()
+print(credentials)
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # credentialrepository
+
+A test for identification, authentication, and authorization against AWS Secrets Manager,
+Azure Key Vault, and GCP Secret Manager.
+
+There are two scenarios to allow access that I'm interested in:
+
+1. Allow access to the service
+2. Allow access to service functions
+3. Allow access to all resources
+4. Allow access to a specific resource
+
+There are three scenarios to deny access that I'm interested in:
+
+1. Deny access to the service
+2. Deny access to service functions
+3. Deny access to all resources
+4. Deny access to a specific resource
+
+These can be paired in the following ways:
+
+# you have no business doing here
+
+1. Deny access to the service
+
+# ok, you're part of the bigger picture, but that doesn't mean you should be able to do anything
+
+1. Allow access to the service
+2. Deny access to all service functions
+3. Deny access to all resources
+
+# let's fence you in
+
+1. Allow access to the service
+2. Allow access to service functions
+3. Allow access to a specific resource
+4. Deny access to a specific resource
+
+# super user
+
+1. Allow access to the service
+2. Allow access to service functions
+3. Allow access to all resources
+
+# testing, testing, 1, 2, 3
+
+To accomplish the above you need 5 accounts:
+
+1. No Account
+2. Organizational Account
+3. User Account #1
+4. User Account #2
+5. Administrative Account
+
+2 resources:
+
+1. Credential Set #1
+2. Credential Set #2
+
+And 3 permission sets:
+
+1. User account #1 has <i>get credential</i> scoped to Credential Set #1
+1. User account #1 has <i>get credential</i> scoped to Credential Set #1
+
+# testing, testing, 1, 2, 3
